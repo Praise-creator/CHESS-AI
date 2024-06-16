c028587bbb26a17c57c07965a81be2549172c31c
Praise-creator <olatide.praise7@gmail.com>
2024-06-16 18:38:09 -0400

    stuff

---
+++ board.py
@@ -0,0 +1,26 @@
+from const import *
+from square import Square
+from piece import *
+
+class board: 
+
+    def __init__(self) :
+        self.squares = [[0, 0, 0, 0, 0, 0, 0, 0] for col in range (COLS)]
+        self._create()
+        
+    def _create(self):
+        for row in range(ROWS):
+            for col in range(COLS):
+                self.squares[row][col] = Square(row,col)
+
+
+    def _add_pieces(self, color):
+        row_pawn, row_other = (6,7) if color == 'white' else (1,0)
+        
+        # pawns
+        for col in range (COLS):
+            self.squares[row_pawn][col] = Square(row_pawn, col, Pawn(color))
+        
+        #knights
+        self.squares[row_other][1] = Square(row_other, col, Knight(color))
+        
--- main.py
+++ main.py
@@ -13,10 +13,11 @@ class Main:
 
     def mainloop(self):
          
-
+        game = self.game
+        screen = self.screen
 
         while True:
-            self.game.show_bg(self.screen)
+            game.show_bg(screen)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
